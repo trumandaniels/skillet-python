2bc93a4eee3d983e0d47a40cd0f667d629b8b18c
Truman Daniels <trumanadaniels@gmail.com>
2026-03-02 15:50:02 -0800

    Security Fixes

--- src/skillet/client.py
+++ src/skillet/client.py
@@ -132,7 +132,7 @@ class Client:
         from skillet import Client
 
         client = Client(
-            api_key="sk_live_AbCdEfGhIjKlMnOpQrStUvWx",
+            api_key="sk_live_AbC...vWx",
             bearer_token="your-bearer-token",
         )
 
